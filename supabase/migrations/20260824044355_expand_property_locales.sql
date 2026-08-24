b18474c27f36888c0e523c67db05ed6aa470c984
crmcloudsales <crmcloudsales@gmail.com>
2026-08-23 23:44:29 -0500

    Remove mismatched LISTIA locale migration filename

--- supabase/migrations/20260824044355_expand_property_locales.sql
+++
@@ -1,2 +0,0 @@
-alter table public.properties drop constraint if exists properties_locale_check;
-alter table public.properties add constraint properties_locale_check check (locale in ('es','en','fr','it','pt-BR','de','ar-AE'));
