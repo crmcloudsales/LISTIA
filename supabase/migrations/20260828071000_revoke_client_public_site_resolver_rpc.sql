ce3ba7b775182fdcae3fb84c52ca32b0bcdd1b98
crmcloudsales <crmcloudsales@gmail.com>
2026-08-28 02:09:56 -0500

    security: revoke client access to public site resolver RPC

---
+++ supabase/migrations/20260828071000_revoke_client_public_site_resolver_rpc.sql
@@ -0,0 +1,2 @@
+revoke execute on function public.resolve_listia_public_site(text) from public, anon, authenticated;
+grant execute on function public.resolve_listia_public_site(text) to service_role;
